47ad168e3c9a2493fddcff438c7da0a758f69108
WangjunY <wangjun.yang@linnworks.com>
2020-12-09 11:57:00 +0200

    Add google tag manager script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,19 @@
 import "normalize.css";
 import { AppProps } from "next/app";
+import Router from 'next/router';
+import React, {useEffect } from 'react';
+import { GTMPageView } from '../utils/gtm';
 // NOTE: Do not move the styles dir to the src.
 // They are used by the Netlify CMS preview feature.
 import "../../public/styles/global.css";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const handleRouteChange = (url: string) => GTMPageView(url);
+    Router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      Router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, []);
   return <Component {...pageProps} />;
 }
